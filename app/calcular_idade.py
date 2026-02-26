6bdaed890839761b551c87d5ff0c95f7160e6149
Wesley Marques <marques_bertin@hotmail.com>
2026-02-26 11:44:29 -0300

    feat: branch QA and more tests

--- app/calcular_idade.py
+++ app/calcular_idade.py
@@ -10,3 +10,7 @@ input_ano_nascimento = int(input("Digite o ano de nascimento: "))
 
 calcular_idade_resultado = calcular_idade(input_ano_nascimento)
 print(f"Sua idade é: {calcular_idade_resultado} anos.")
+
+
+def count_characters(text):
+    return len(text)
